49896886126abc75c13997d302df42af98c453d8
Chun Wong <98971821+Misaya7@users.noreply.github.com>
2022-11-02 22:35:45 +0800

    Add files via upload

--- 22.11.2/22-11-2.c
+++ 22.11.2/22-11-2.c
@@ -67,12 +67,12 @@
 //}
 
 
-////带头结点的单链表
-//typedef struct Lnode {
-//    int data;
-//    struct Lnode* next;
-//}Lnode;
-//bool Initlist(Lnode* p) {
+//带头结点的单链表
+typedef struct Lnode {
+    int data;
+    struct Lnode* next;
+}Lnode;
+//bool Initlist(Lnode* p) {//尾插法初始化单链表
 //    p->next = NULL;
 //    Lnode* r = p;//尾指针
 //    printf("你要初始化多少个数:>");
@@ -93,134 +93,173 @@
 //    printf("录入成功!\n");
 //    return true;
 //}
-//bool Emptylist(Lnode* p) {
-//    if (p->next == NULL) {
-//        return true;
+
+bool Initlist(Lnode* p) {//头插法初始化单链表
+    p->next = NULL;
+    printf("你要初始化多少个数:>");
+    int in = 0;
+    scanf("%d", &in);
+
+    int num = 1;
+    while (in--) {
+        Lnode* n = (Lnode*)malloc(sizeof(Lnode));
+        printf("第%d个值为:>", num);
+        scanf("%d", &n->data);
+        n->next = p->next;
+        p->next = n;
+        num++;
+    }
+    printf("录入成功!\n");
+    return true;
+}
+bool Reverse(Lnode* p,Lnode* n) {
+        n->next = NULL;
+        while (p->next) {
+            Lnode* s = (Lnode*)malloc(sizeof(Lnode));
+            p = p->next;
+            s->data = p->data;
+            s->next = n->next;
+            n->next = s;
+        }
+    return true;
+}
+bool Emptylist(Lnode* p) {
+    if (p->next == NULL) {
+        return true;
+    }
+    return false;
+}
+Lnode* Findlast(Lnode* p, int i) {
+    if (i < 1) {
+        return 0;
+    }
+    Lnode* q = p;//q从头指针p开始扫描,q指向现在扫描到的结点
+    int j = 0;//当前q指向的是第几个结点
+    while (j < i - 1 && q != NULL) {//q找到第i-1个结点
+        q = q->next;
+        j++;
+    }
+    return q;
+}
+bool Insertlist(Lnode* p, int i,int e) {//在p中的第i个位置插入e
+    //if (i < 1) {
+    //    return false;
+    //}
+    //Lnode* q = p;//q从头指针p开始扫描,q指向现在扫描到的结点
+    //int j = 0;//当前q指向的是第几个结点
+    //while (j < i - 1 && q != NULL) {//q找到第i-1个结点
+    //    q = q->next;
+    //    j++;
+
 //    }
-//    return false;
-//}
-//Lnode* Findlast(Lnode* p, int i) {
-//    if (i < 1) {
-//        return 0;
-//    }
-//    Lnode* q = p;//q从头指针p开始扫描,q指向现在扫描到的结点
-//    int j = 0;//当前q指向的是第几个结点
-//    while (j < i - 1 && q != NULL) {//q找到第i-1个结点
-//        q = q->next;
-//        j++;
-//    }
-//    return q;
-//}
-//bool Insertlist(Lnode* p, int i,int e) {//在p中的第i个位置插入e
-//    //if (i < 1) {
-//    //    return false;
-//    //}
-//    //Lnode* q = p;//q从头指针p开始扫描,q指向现在扫描到的结点
-//    //int j = 0;//当前q指向的是第几个结点
-//    //while (j < i - 1 && q != NULL) {//q找到第i-1个结点
-//    //    q = q->next;
-//    //    j++;
-//
-////    }
-//    Lnode* q = Findlast(p, i);
-//    
-//    if (q == NULL) {
-//        return false;
-//    }
-//
-//    Lnode* s = (Lnode*)malloc(sizeof(Lnode));
-//    s->data = e;
-//    s->next = q->next;
-//    q->next = s;
-//    return true;
-//}
-//bool Dellist(Lnode* p, int i,int *e) {
-//    Lnode* q = Findlast(p, i);
-//    //if (i < 1) {
-//    //    return false;
-//    //}
-//    //Lnode* q = p;//q从头指针p开始扫描,q指向现在扫描到的结点
-//    //int j = 0;//当前q指向的是第几个结点
-//    //while (j < i - 1 && q != NULL) {//q找到第i-1个结点
-//    //    q = q->next;
-//    //    j++;
-//    //}
-//    if (q == NULL) {
-//        return false;
-//    }
-//    if (p->next == NULL) {
-//        return false;
-//    }
-//    Lnode* u = q->next;
-//    *e = u->data;
-//    q->next = u->next;
-//    free(u);
-//    u = NULL;
-//    return true;
-//}
-//bool Showlist(Lnode* p) {
-//    if (p->next == NULL) {
-//        return false;
-//    }
-//    
-//    while (p->next!=NULL) {
-//        p = p->next;
-//        printf("%d ", p->data);
-//    }
-//    printf("\n");
-//    return true;
-//}
-//Lnode* Getelsm(Lnode* p, int i) {//按位查找
-//    Lnode* q = Findlast(p, i);//找到第i-1个结点
-//    if (q== NULL)
-//        return NULL;
-//
-//    q = q->next;
-//    if (q != NULL)
-//        return q;
-//    else
-//        return NULL;
-//}
-//int Length(Lnode* p) {
-//    int len = 0;
-//    Lnode* q = p->next;
-//    while (q != NULL) {
-//        q = q->next;
-//        len++;
-//    }
-//    return len;
-//}
-//int main() {
-//    Lnode* head = (Lnode*)malloc(sizeof(Lnode));
-//    Initlist(head);
-//    //if (Emptylist(head)) {
-//    //    printf("空表\n");
-//    //}
-//    //else {
-//    //    printf("不为空\n");
-//
-//    Insertlist(head, 3, 9);
-//    printf("插入%d成功\n", 9);
-//
-//    Showlist(head);
-//
-//    printf("长度为:%d\n", Length(head));
-//
-//    int e = 0;
-//    Dellist(head,1,&e);
-//    printf("删除了%d\n", e);
-//
-//    Showlist(head);
-//
-//    Lnode* o = Getelsm(head, 3);
-//    if (o != NULL)
-//    printf("%d\n", o->data);
-//    else {
-//        printf("没有找到\n");
-//    }
-//
-//    printf("长度为:%d\n", Length(head));
-//}
+    Lnode* q = Findlast(p, i);
+    
+    if (q == NULL) {
+        return false;
+    }
+
+    Lnode* s = (Lnode*)malloc(sizeof(Lnode));
+    s->data = e;
+    s->next = q->next;
+    q->next = s;
+    return true;
+}
+bool Dellist(Lnode* p, int i,int *e) {
+    Lnode* q = Findlast(p, i);
+    //if (i < 1) {
+    //    return false;
+    //}
+    //Lnode* q = p;//q从头指针p开始扫描,q指向现在扫描到的结点
+    //int j = 0;//当前q指向的是第几个结点
+    //while (j < i - 1 && q != NULL) {//q找到第i-1个结点
+    //    q = q->next;
+    //    j++;
+    //}
+    if (q == NULL) {
+        return false;
+    }
+    if (p->next == NULL) {
+        return false;
+    }
+    Lnode* u = q->next;
+    *e = u->data;
+    q->next = u->next;
+    free(u);
+    u = NULL;
+    return true;
+}
+bool Showlist(Lnode* p) {
+    if (p->next == NULL) {
+        return false;
+    }
+    
+    while (p->next!=NULL) {
+        p = p->next;
+        printf("%d ", p->data);
+    }
+    printf("\n");
+    return true;
+}
+Lnode* Getelsm(Lnode* p, int i) {//按位查找
+    Lnode* q = Findlast(p, i);//找到第i-1个结点
+    if (q== NULL)
+        return NULL;
+
+    q = q->next;
+    if (q != NULL)
+        return q;
+    else
+        return NULL;
+}
+int Length(Lnode* p) {
+    int len = 0;
+    Lnode* q = p->next;
+    while (q != NULL) {
+        q = q->next;
+        len++;
+    }
+    return len;
+}
+int main() {
+    Lnode* head = (Lnode*)malloc(sizeof(Lnode));
+    Initlist(head);
+    Showlist(head);
+    //if (Emptylist(head)) {
+    //    printf("空表\n");
+    //}
+    //else {
+    //    printf("不为空\n");
+
+    Insertlist(head, 3, 9);
+    printf("插入%d成功\n", 9);
+
+    Showlist(head);
+
+    Lnode* n = (Lnode*)malloc(sizeof(Lnode));
+    Reverse(head, n);
+    Showlist(n);
+
+    printf("长度为:%d\n", Length(head));
+
+    int e = 0;
+    Dellist(head,1,&e);
+    printf("删除了%d\n", e);
+
+    Showlist(head);
+
+    Lnode* o = Getelsm(head, 3);
+    if (o != NULL)
+    printf("%d\n", o->data);
+    else {
+        printf("没有找到\n");
+    }
+
+    printf("长度为:%d\n", Length(head));
+
+    n = (Lnode*)malloc(sizeof(Lnode));
+    Reverse(head,n);
+    Showlist(n);
+}
 
 ////给十进制出二进制
 //int main() {
@@ -245,19 +284,19 @@
 //}
 
 //递归得到二进制
-void er(int input) {
-    if (input) {
-        er(input / 2);
-    }
-    if (input == 0)
-        return;
-    printf("%d", input % 2);
-}
-
-int main() {
-    int input = 0;
-    scanf("%d", &input);
-    er(input);
-
-    return 0;
-}
+//void er(int input) {
+//    if (input) {
+//        er(input / 2);
+//    }
+//    if (input == 0)
+//        return;
+//    printf("%d", input % 2);
+//}
+//
+//int main() {
+//    int input = 0;
+//    scanf("%d", &input);
+//    er(input);
+//
+//    return 0;
+//}
